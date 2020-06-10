cea8a6f2229f3a46f6e4464a1c49eebcc572f002
Mihir Lad <mihirlad55@gmail.com>
2020-06-09 22:50:52 -0400

    Add status formatting options
    This removes the hardcoded format constants.
    A new enum for storing the program mode is created.
    The format_output function is completely rewritten to incorporate user-specified
    formatting options such as max_artist_length, max_title_length, max_length,
    status_format, and trunc string.
    
    The status_format specifies how to output the spotify status where the %artist%
    and %title% tokens are replaced by the artist name and track title,
    respectively.
    
    The trunc string is what the artist name or track title ends with if its been
    truncated.
    
    The get_status function passes along the above formatting options.
    
    A new command-line parsing code is added which allows the user to specify
    options and the command in any order.
    
    Since the commandline options are now processed altogether, no dbus connections
    are made until after the commandline options have been parsed now.

--- src/spotifyctl.c
+++ src/spotifyctl.c
@@ -24,12 +24,15 @@ const char *PLAYER_METHOD_PREVIOUS = "Previous";
 const char *METADATA_TITLE_KEY = "xesam:title";
 const char *METADATA_ARTIST_KEY = "xesam:artist";
 
-const int MAX_OUTPUT_LENGTH = 30;
-// Leave 3 characters for "..." if cut off and 2 for ": "
-const int OUTPUT_PADDING_LENGTH = 5;
-// Title + artist length should be less than max output - padding
-const int MAX_TITLE_LENGTH = 15;
-const int MAX_ARTIST_LENGTH = 10;
+typedef enum {
+    MODE_NONE,
+    MODE_STATUS,
+    MODE_PLAY,
+    MODE_PAUSE,
+    MODE_PREVIOUS,
+    MODE_NEXT,
+    MODE_PLAYPAUSE
+} ProgMode;
 
 dbus_bool_t SUPPRESS_ERRORS = 0;
 
@@ -68,35 +71,34 @@ char *get_song_artist_from_metadata(DBusMessage *msg) {
     return artist;
 }
 
-char *format_output(char *artist, char *title) {
-    size_t title_len = strlen(title);
-    size_t artist_len = strlen(artist);
+char *format_output(char *artist, char *title, int max_artist_length,
+                    int max_title_length, int max_length, char *format,
+                    char *trunc) {
+    // Truncate artist and track title using the truncation string
+    str_trunc(title, max_title_length, trunc);
+    str_trunc(artist, max_artist_length, trunc);
 
-    // +2 = "\n\0"
-    const size_t OUTPUT_SIZE = MAX_OUTPUT_LENGTH + 2;
+    // Replace all tokens with their values
+    char *temp = str_replace_all(format, "%artist%", artist);
+    char *output = str_replace_all(temp, "%title%", title);
 
-    char *output = (char *)calloc(OUTPUT_SIZE, sizeof(char));
-    output[0] = '\0';
+    // Truncate output to max length
+    str_trunc(output, max_length, "");
 
-    if (title_len + artist_len > MAX_OUTPUT_LENGTH) {
-        if (title_len > MAX_TITLE_LENGTH) title_len = MAX_TITLE_LENGTH;
-        if (artist_len > MAX_ARTIST_LENGTH) artist_len = MAX_ARTIST_LENGTH;
+    // Allocate extra character to add newline
+    const size_t OUTPUT_SIZE = (strlen(output) + 1) * sizeof(char);
+    output = (char *)realloc(output, OUTPUT_SIZE);
 
-        strncpy(output, artist, artist_len);
-        strcat(output, ": ");
-        strncat(output, title, title_len);
-        strcat(output, "...\n");
-    } else {
-        strncpy(output, artist, artist_len);
-        strcat(output, ": ");
-        strncat(output, title, title_len);
-        strcat(output, "\n");
-    }
+    strcat(output, "\n");
+
+    free(temp);
 
     return output;
 }
 
-void get_status(DBusConnection *connection) {
+void get_status(DBusConnection *connection, int max_artist_length,
+                int max_title_length, int max_length, char *format,
+                char *trunc) {
     DBusError err;
     dbus_error_init(&err);
 
@@ -120,7 +122,8 @@ void get_status(DBusConnection *connection) {
     char *title = get_song_title_from_metadata(reply);
     char *artist = get_song_artist_from_metadata(reply);
 
-    char *output = format_output(artist, title);
+    char *output = format_output(artist, title, max_artist_length,
+                                 max_title_length, max_length, format, trunc);
 
     printf("%s", output);
 
@@ -154,15 +157,66 @@ void print_usage() {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc == 3 && strcmp(argv[1], "-q") == 0) {
-        SUPPRESS_ERRORS = 1;
-    } else if (argc != 2) {
-        print_usage();
-        return 1;
-    }
-
     DBusConnection *connection;
     DBusError err;
+    ProgMode prog_mode = MODE_NONE;
+
+    int max_artist_length = 10;
+    int max_title_length = 15;
+    int max_length = 30;
+    char *status_format = "%artist%: %title%";
+    char *trunc = "...";
+
+    for (size_t i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-q") == 0) {
+            SUPPRESS_ERRORS = 1;
+        } else if (strcmp(argv[i], "--max-artist-length") == 0) {
+            max_artist_length = atoi(argv[++i]);
+            if (max_artist_length == 0) {
+                fputs("Artist length must be a positive integer!", stderr);
+                print_usage();
+                return 1;
+            }
+        } else if (strcmp(argv[i], "--max-title-length") == 0) {
+            max_title_length = atoi(argv[++i]);
+            if (max_title_length == 0) {
+                fputs("Title length must be a positive integer!", stderr);
+                print_usage();
+                return 1;
+            }
+        } else if (strcmp(argv[i], "--max-length") == 0) {
+            max_length = atoi(argv[++i]);
+            if (max_length == 0) {
+                fputs("Max length must be a positive integer!", stderr);
+                print_usage();
+                return 1;
+            }
+        } else if (strcmp(argv[i], "--format") == 0) {
+            status_format = argv[++i];
+        } else if (strcmp(argv[i], "--trunc") == 0) {
+            trunc = argv[++i];
+        } else if (strcmp(argv[i], "status") == 0) {
+            prog_mode = MODE_STATUS;
+        } else if (strcmp(argv[i], "play") == 0) {
+            prog_mode = MODE_PLAY;
+        } else if (strcmp(argv[i], "pause") == 0) {
+            prog_mode = MODE_PAUSE;
+        } else if (strcmp(argv[i], "playpause") == 0) {
+            prog_mode = MODE_PLAYPAUSE;
+        } else if (strcmp(argv[i], "next") == 0) {
+            prog_mode = MODE_NEXT;
+        } else if (strcmp(argv[i], "previous") == 0) {
+            prog_mode = MODE_PREVIOUS;
+        } else if (strcmp(argv[i], "help") == 0) {
+            print_usage();
+            return 0;
+        } else {
+            fprintf(stderr, "Invalid option '%s'\n", argv[i]);
+            fputs("usage: spotifyctl [ -q ] [options] <command>\n", stderr);
+            fputs("Try 'spotifyctl help' for more information\n", stderr);
+            return 1;
+        }
+    }
 
     dbus_error_init(&err);
 
@@ -172,22 +226,37 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    if (strcmp(argv[argc - 1], "status") == 0) {
-        get_status(connection);
-    } else if (strcmp(argv[argc - 1], "play") == 0) {
-        spotify_player_call(connection, PLAYER_METHOD_PLAY);
-    } else if (strcmp(argv[argc - 1], "pause") == 0) {
-        spotify_player_call(connection, PLAYER_METHOD_PAUSE);
-    } else if (strcmp(argv[argc - 1], "playpause") == 0) {
-        spotify_player_call(connection, PLAYER_METHOD_PLAYPAUSE);
-    } else if (strcmp(argv[argc - 1], "next") == 0) {
-        spotify_player_call(connection, PLAYER_METHOD_NEXT);
-    } else if (strcmp(argv[argc - 1], "previous") == 0) {
-        spotify_player_call(connection, PLAYER_METHOD_PREVIOUS);
-    } else {
-        print_usage();
-        dbus_connection_unref(connection);
-        return 1;
+    switch (prog_mode) {
+        case MODE_NONE:
+            fputs("No command specified\n", stderr);
+            fputs("Try 'spotifyctl help' for more information\n", stderr);
+            dbus_connection_unref(connection);
+            return 1;
+
+        case MODE_STATUS:
+            get_status(connection, max_artist_length, max_title_length,
+                       max_length, status_format, trunc);
+            break;
+
+        case MODE_PLAY:
+            spotify_player_call(connection, PLAYER_METHOD_PLAY);
+            break;
+
+        case MODE_PAUSE:
+            spotify_player_call(connection, PLAYER_METHOD_PAUSE);
+            break;
+
+        case MODE_PLAYPAUSE:
+            spotify_player_call(connection, PLAYER_METHOD_PLAYPAUSE);
+            break;
+
+        case MODE_NEXT:
+            spotify_player_call(connection, PLAYER_METHOD_NEXT);
+            break;
+
+        case MODE_PREVIOUS:
+            spotify_player_call(connection, PLAYER_METHOD_PREVIOUS);
+            break;
     }
 
     dbus_connection_unref(connection);
